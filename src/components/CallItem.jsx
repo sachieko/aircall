8bb7d9e10f1c101170189567da90dd8d26bfcc20
sachieko <arashikabane@gmail.com>
2023-01-31 20:13:26 -0700

    remove old application

--- src/components/CallItem.jsx
+++
@@ -1,7 +0,0 @@
-const CallItem = () => {
-  return (
-    <div className="call-item">This is a call item</div>
-  );
-};
-
-export default CallItem;
--- src/components/CallItemDetail.jsx
+++
@@ -1,7 +0,0 @@
-const CallItemDetail = () => {
-  return (
-    <div className="call-item-detailed">This is a detailed call item</div>
-  );
-};
-
-export default CallItemDetail;
