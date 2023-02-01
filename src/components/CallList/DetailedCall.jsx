1cc19db5e03fb4f5f91538782de384680977fb4e
sachieko <arashikabane@gmail.com>
2023-02-01 09:48:37 -0700

    Fix click handler issue where archiving also opens detailed view

--- src/components/CallList/DetailedCall.jsx
+++ src/components/CallList/DetailedCall.jsx
@@ -4,9 +4,9 @@ const DetailedCall = function({
     number, direction, time, isArchiving, is_archived, handleArchive, onClick, via, callType, duration 
   }) {
   return (
-    <div className="call-item--detailed" onClick={onClick}>
+    <div className="call-item--detailed" >
       <div className="call-info">
-        <div className="call-number">{direction === 'inbound' ? '↙️' : '↗️'} <span>{number}</span></div>
+        <div className="call-number" onClick={onClick}>{direction === 'inbound' ? '↙️' : '↗️'} <span>{number}</span></div>
         <div className="call-time">{time}</div>
         <div className="call-status">
           {!isArchiving && !is_archived && <Button text={'Archive'} onClick={handleArchive} />}
@@ -15,7 +15,7 @@ const DetailedCall = function({
           {isArchiving && is_archived && <div>Unarchiving..</div>}
         </div>
       </div>
-      <div className="call-details">
+      <div className="call-details" onClick={onClick}>
         <div className="call-duration">Call lasted {duration} seconds.</div>
         <div className="call-type">Type: {callType}</div>
         {via && <div className="call-via">Aircall#: {via}</div>}
--- src/components/CallList/PlainCall.jsx
+++ src/components/CallList/PlainCall.jsx
@@ -2,8 +2,8 @@ import Button from "../Button";
 
 const PlainCall = function({ number, direction, time, isArchiving, is_archived, handleArchive, onClick }) {
   return (
-    <div className="call-item" onClick={onClick}>
-      <div className="call-number">{direction === 'inbound' ? '↙️' : '↗️'} <span>{number}</span></div>
+    <div className="call-item">
+      <div className="call-number" onClick={onClick}>{direction === 'inbound' ? '↙️' : '↗️'} <span>{number}</span></div>
       <div className="call-time">{time}</div>
       <div className="call-status">
         {!isArchiving && !is_archived && <Button text={'Archive'} onClick={handleArchive} />}
