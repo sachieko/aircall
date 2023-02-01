dfe3f94cc81a864fa5b4fb368d744016241b4c1a
sachieko <arashikabane@gmail.com>
2023-02-01 11:06:18 -0700

    Add scroll to top functionality for header on click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import '../styles/Header.scss';
 
 const Header = () => {
+  const toTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <header>
+    <header onClick={toTop}>
       <svg width='486px' height='168px' viewBox='0 0 486 168' version='1.1' xmlns='http://www.w3.org/2000/svg'>
         <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
           <g transform='translate(207.000000, 24.000000)'>
