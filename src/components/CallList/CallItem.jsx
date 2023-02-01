a4f8d9db586171e51a0160688824e195b4bd2a97
sachieko <arashikabane@gmail.com>
2023-02-01 07:23:09 -0700

    Remove scrollbar, missing numbers display as private

--- src/components/CallList/CallItem.jsx
+++ src/components/CallList/CallItem.jsx
@@ -25,7 +25,7 @@ const CallItem = function({ call, archiveToggle }) {
 
   return (
     <div className="call-item">
-    <div className="call-number">{from || to}</div>
+    <div className="call-number">{from || to || 'Private'}</div>
     <div className={`call-icon ${direction === 'inbound' ? 'inbound-icon' : 'outbound-icon'}`}></div>
     <div className="call-time">{created_at.substring(11,16)}</div>
     {isArchiving && <div>Archiving..</div>}
